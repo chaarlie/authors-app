7b5f44941e14dd3f595fb74fe9662a93537b0fe5
Carlos De la Rosa <carlos_de_la_rosa@claro.com.do>
2019-01-29 16:40:12 -0400

    making all fields auto-editable

--- Documents/sweatworks-project/authorsApp/src/app/shared/editable-input.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EditableInputDirective } from './editable-input.directive';
-
-describe('EditableInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EditableInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Documents/sweatworks-project/authorsApp/src/app/shared/editable-input.directive.ts
+++ Documents/sweatworks-project/authorsApp/src/app/shared/editable-input.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 import { AuthorService } from '../component/authors/author.service';
 import { Author } from '../component/authors/model/Author';
+import { Publication } from '../component/publications/model/Publication';
 
 @Directive({
   selector: '[appEditableInput]'
@@ -11,14 +12,38 @@ export class EditableInputDirective {
 
   @Input() attributeName: string;
   @Input() author: Author;
+  @Input() publicationOb: Publication;
 
   @HostListener('click') onclick() {
     this.ref.nativeElement.contentEditable = true;
   }
 
   @HostListener('blur') onBlur() {
-    if(this.author.hasOwnProperty(this.attributeName)) {
-      this.author[this.attributeName] = this.ref.nativeElement.innerHTML;
+    if( this.publicationOb && this.publicationOb.hasOwnProperty(this.attributeName) ) {
+      this.authorService.getAll().subscribe((authors) => {
+      let modifiedAuthor = authors.filter((a) => a.id == this.author.id );
+      let { publications } = modifiedAuthor[0];
+        
+      for(let k in publications) {
+        if(
+          publications[k].title == this.publicationOb.title ||
+          publications[k].date == this.publicationOb.date ||
+          publications[k].body == this.publicationOb.body 
+          ) {
+            
+            publications[k][this.attributeName] = this.ref.nativeElement.innerText;
+  
+            break;
+        }
+      }
+
+      modifiedAuthor[0].publications = publications;
+      this.authorService.update(modifiedAuthor[0]);
+      });
+    }
+
+    if(this.author && this.author.hasOwnProperty(this.attributeName) ) {
+      this.author[this.attributeName] = this.ref.nativeElement.childNodes[0].innerText;
       this.authorService.update(this.author);
     }
   }
